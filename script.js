e358a44b6fb2350ea66546396b7f07362f29f25b
Aeron A. Supremido <aeronsupremido1@gmail.com>
2026-09-02 21:02:04 +0800

    add calculation

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateItemAmount(price, quantity) {
+    return price * quantity;
+}
+
+function calculateDiscount(subtotal) {
+    let discountRate = 0;
+
+    if (subtotal >= 5000) {
+        discountRate = 0.10;
+    } else if (subtotal >= 3000) {
+        discountRate = 0.07;
+    } else if (subtotal >= 1000) {
+        discountRate = 0.05;
+    } else {
+        discountRate = 0;
+    }
+
+    return subtotal * discountRate;
+}
